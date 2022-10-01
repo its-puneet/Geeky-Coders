36c94859c617d445327b278fa991b1519dd75297
Carsent <114802599+Carsent@users.noreply.github.com>
2022-10-01 14:28:01 +0530

    Testimonial Slider

---
+++ Testimonial Slider/script.js
@@ -0,0 +1,32 @@
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+console.log(slides);
+
+function display(index) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
+}
+
+function nextSlide() {
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  display(index);
+}
+
+function prevSlide() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  display(index);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+display(index);
