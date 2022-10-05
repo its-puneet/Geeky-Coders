ff272155133562db0f9f0ee5968a5ddb31b6216c
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-05 11:07:17 +0530

    Navbar formatted

--- Star Wars/src/components/NavBar.jsx
+++ Star Wars/src/components/NavBar.jsx
@@ -8,23 +8,17 @@ import SegmentIcon from '@mui/icons-material/Segment';
 import CloseIcon from '@mui/icons-material/Close';
 
 function Navbar() {
-
     const [theme, setTheme] = useState('light')
-
-
     const [open, setOpen] = useState(false)
-
     const toggleTheme = () => {
         setTheme(theme === 'light' ? 'dark' : 'light')
         document.body.classList.toggle('dark')
     }
-
     const toogleMenu = (e) => {
         const menu = document.querySelector('#menu-list')
         menu.classList.toggle('hidden')
         setOpen(!open)
     }
-
     return (
         <>
             <div className='lg:flex   flex-row justify-between  lg:items-center lg:justify-between p-5  shadow-black shadow-sm bg-white dark:bg-gray-800 bg-opacity-90  dark:shadow-slate-50  sticky top-0  dark:bg-bg-dark dark:text-white z-50'>
@@ -32,15 +26,12 @@ function Navbar() {
                     <div className='inline '>
                         <Link to={`/`} className='text-2xl font-bold text-black dark:text-white'>Star Wars ⚠️</Link>
                     </div>
-
                     <div className="">
                         <span className=' lg:hidden text-3xl'>
                             {open ? <CloseIcon onClick={toogleMenu} className='cursor-pointer menu-options' /> : <SegmentIcon onClick={toogleMenu} className='cursor-pointer menu-options text-6xl' />}
                         </span>
                     </div>
-
                 </div>
-
                 <div className="links dark:bg-bg-dark  lg:flex flex-row hidden" id="menu-list">
                     <ul className='lg:flex lg:flex-row lg:justify-end lg:opacity-100 opacity-100 text-center flex-col   p-10 gap-6 sm:flex   lg:py-0  lg:pl-0 '>
                         <li><a href="#" ><Link to={'/'} className="nav-list   hover:font-semibold " >Films</Link></a></li>
@@ -56,7 +47,6 @@ function Navbar() {
                         </label>
                         <span className='pl-2'>{theme === 'light' ? <Brightness5Icon /> : <Brightness2Icon />}</span>
                     </ul>
-
                 </div>
             </div>
         </>
