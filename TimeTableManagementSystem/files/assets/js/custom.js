230a9a1f4a5643708ddf77b94c8f6bb7993e3bec
vishsirohi <112557174+vishsirohi@users.noreply.github.com>
2022-10-11 19:23:43 +0530

    Add files via upload

---
+++ TimeTableManagementSystem/files/assets/js/custom.js
@@ -0,0 +1,74 @@
+﻿/*=============================================================
+ Authour URI: www.binarytheme.com
+ License: Commons Attribution 3.0
+
+ http://creativecommons.org/licenses/by/3.0/
+
+ 100% Free To use For Personal And Commercial Use.
+ IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+
+ ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scrollAnimation_fun: function () {
+
+            /*====================================
+             ON SCROLL ANIMATION SCRIPTS 
+             ======================================*/
+
+
+            window.scrollReveal = new scrollReveal();
+
+        },
+        scroll_fun: function () {
+
+            /*====================================
+             EASING PLUGIN SCRIPTS 
+             ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+        },
+
+        top_flex_slider_fun: function () {
+            /*====================================
+             FLEX SLIDER SCRIPTS 
+             ======================================*/
+            $('#main-section').flexslider({
+                animation: "fade", //String: Select your animation type, "fade" or "slide"
+                slideshowSpeed: 3000,           //Integer: Set the speed of the slideshow cycling, in milliseconds
+                animationSpeed: 1000,           //Integer: Set the speed of animations, in milliseconds
+                startAt: 0,    //Integer: The slide that the slider should start on. Array notation (0 = first slide)
+
+            });
+        },
+
+        custom_fun: function () {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+             ======================================*/
+
+
+        },
+
+    }
+
+
+    $(document).ready(function () {
+        mainApp.scrollAnimation_fun();
+        mainApp.scroll_fun();
+        mainApp.top_flex_slider_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
