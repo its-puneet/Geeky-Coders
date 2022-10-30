561dff9609d6115daf6cf61279b1940dc4bcf737
Anushkaiitmm <72934052+Anushkaiitmm@users.noreply.github.com>
2022-10-31 00:03:17 +0530

    Update function.js

--- Age calculator/function.js
+++ Age calculator/function.js
@@ -1,9 +1,4 @@
-function age() {
-    var d1 = document.getElementById("date").value;
-    var m1 = document.getElementById("month").value;
-    var y1 = document.getElementById("year").value;
-    var date = new Date();
-    var d2 = date.getDate();
+
     var m2 = 1 + date.getMonth();
     var y2 = date.getFullYear();
     var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
